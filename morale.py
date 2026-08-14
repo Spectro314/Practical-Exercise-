e96269066606ab669b654677c2e66023466f1581
dharr4 <dnharrington@proton.me>
2026-08-14 14:02:39 -0400

    dh_commit2

---
+++ morale.py
@@ -0,0 +1,9 @@
+import random
+
+jokes = [
+    "Why don't scientists trust atoms? Because they make up everything! 😂",
+    "Why did the scarecrow win an award? Because he was outstanding in his field! 🌾",
+    "What do you call fake spaghetti? An impasta! 🍝"
+]
+
+print(random.choice(jokes))
